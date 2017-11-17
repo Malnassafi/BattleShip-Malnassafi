83290374dfb30e50f4a98ad9a965f9a530668eba
Malnassafi <malnassafi@mail.csuchico.edu>
2017-11-16 23:46:32 -0800

    almost done

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 const int NUM_OF_ROWS = 11;
 const int NUM_OF_COLS = 11;
-//const int SHIP_TYPES = 4;
+const int MAX_NUM_OF_SHIPS = 8;
 
 const char IS_HIT = '!';
 const char IS_WATER = '_';
@@ -17,37 +17,41 @@ const char IS_SHIP = 'S';
 char PLAYER_BOARD[NUM_OF_ROWS][NUM_OF_COLS];
 char COMPUTERS_BOARD[NUM_OF_ROWS][NUM_OF_COLS];
 
-bool readyToPlay();
+bool readyToPlay(string &);
 void displayBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string);
-//void placeYourShips(char & array[NUM_OF_ROWS][NUM_OF_COLS]);
-//void displayComputersBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS]);
-
+void placeYourShips(char array[NUM_OF_ROWS][NUM_OF_COLS], string);
+void computerRandomGenerateShips();
 int main()
 {
         string Player, Computer;
         bool play;
-        play = readyToPlay();
+        play = readyToPlay(Player);
         if(play == true)
         {
                 displayBoard(PLAYER_BOARD, Player);
-                displayBoard(COMPUTERS_BOARD, Computer);
+                displayBoard(COMPUTERS_BOARD, "Computer");
+                placeYourShips(PLAYER_BOARD, Player);
+                computerRandomGenerateShips();
         }
         return 0;
 }
 
-bool readyToPlay()
+bool readyToPlay(string & name)
 {   
         char cheak;
-        cout<<"********************************\n";
+        cout<<"\n********************************\n";
         cout<<"********************************\n";
         cout<<"*********BattleShipGame*********\n";
         cout<<"********************************\n";
         cout<<"********************************\n";
-        cout<<"\n The game goes as followes:\n _ stand for the water,\n S stand for your ships,\n ! stands for the ships that you or the Computer hit,\n O stands for every time you or the Computer misses \n"; 
+        cout<<"\n The game goes as followes:\n Whoever destroyes all the others ships win!\n";
+        cout<<"_ stands for the water,\n S stands for your ships,\n ! stands for the ships that you or the Computer hit,\n O stands for every time you or the Computer misses \n"; 
         cout<<"ARE YOU READY TO DESTROY YOUR ENEMY? Y/N\n";
         cin>>cheak;
         if(cheak  == 'y' || cheak == 'Y')
         { 
+                cout<<"PLEASE ENETER YOUR NAME BRAVE SAILER\n";
+                cin>>name;
                 return true;
         }
         else
@@ -56,11 +60,42 @@ bool readyToPlay()
         }
 }
 
-/*void placeYourShips(char & array[NUM_OF_ROWS][NUM_OF_COLS])
+void placeYourShips(char array[NUM_OF_ROWS][NUM_OF_COLS], string name)
 {
+        int row, col, k = 0;
+        while(k < MAX_NUM_OF_SHIPS)
+        {
+                cout<<"Please enter the location you would like to put your ships (exp 2 3) (dont forget the max num of ships is 8!)\n";
+                cin >> row >> col ;
+                if(row > 1 && row <= NUM_OF_ROWS && col > 1 && col <= NUM_OF_COLS)
+                {
+                        if(array[row][col] == IS_WATER)
+                        {
+                                array[row][col] = IS_SHIP;
+                                k++;
+                        }
+                        else
+                        {
+                                cerr<<"There is already a ship in that location!(please try again)\n\n";
+                        }
+                }
+                else
+                {
+                        cerr<<"The location you choose is out of bound please choose another one withen the board!\n\n";
+                }
+        }
 
+        cout<<"\n     - "<<name<<"'s Board -\n";
+        for (int r = 0; r < NUM_OF_ROWS; r++)
+        {
+                for (int c = 0; c < NUM_OF_COLS; c++)
+                {
+                        cout<<PLAYER_BOARD[r][c]<<"  ";
+                }
+                cout<<endl;
+        }
 }
-*/
+
 void displayBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string name)
 {
         array[0][0] = '*';
@@ -86,7 +121,7 @@ void displayBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string name)
         array[8][0] = 'H';
         array[9][0] = 'I';
         array[10][0] = 'J'; 
-        
+
         for (int r = 1; r < NUM_OF_ROWS; r++)
         {       
                 for (int c = 1; c < NUM_OF_COLS; c++)
@@ -94,8 +129,8 @@ void displayBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string name)
                         array[r][c] = IS_WATER;
                 }
         }
-        
-        cout<<"             - "<<name<<"-\n";
+
+        cout<<"\n     - "<<name<<"'s Board -\n";
         for (int r = 0; r < NUM_OF_ROWS; r++)
         {       
                 for (int c = 0; c < NUM_OF_COLS; c++)
@@ -107,53 +142,30 @@ void displayBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string name)
 
 }
 
-/*void displayComputersBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS])
+void computerRandomGenerateShips()
 {
-        array[0][0] = '*';
-        //Coloums
-        array[0][1] = '0';
-        array[0][2] = '1';
-        array[0][3] = '2';
-        array[0][4] = '3';
-        array[0][5] = '4';
-        array[0][6] = '5';
-        array[0][7] = '6';
-        array[0][8] = '7';
-        array[0][9] = '8';
-        array[0][10] = '9';
-        //Rows
-        array[1][0] = 'A';
-        array[2][0] = 'B';
-        array[3][0] = 'C';
-        array[4][0] = 'D';
-        array[5][0] = 'E';
-        array[6][0] = 'F';
-        array[7][0] = 'G';
-        array[8][0] = 'H';
-        array[9][0] = 'I';
-        array[10][0] = 'J';
-        
-        for (int r = 1; r < NUM_OF_ROWS; r++)
+        int r, c, k = 0;
+        while(k < MAX_NUM_OF_SHIPS)
         {
-                for (int c = 1; c < NUM_OF_COLS; c++)
+                r = rand() % NUM_OF_ROWS;
+                c = rand() % NUM_OF_COLS;
+                if(COMPUTERS_BOARD[r][c] != 'S')
                 {
-                        array[r][c] = IS_WATER;
+                        COMPUTERS_BOARD[r][c] = 'S';
+                        k++;
                 }
         }
-        cout<<"             -Computer-\n";
+        
+        cout<<"\n     - Computer's Board -\n";
         for (int r = 0; r < NUM_OF_ROWS; r++)
         {
                 for (int c = 0; c < NUM_OF_COLS; c++)
                 {
-                        cout<<array[r][c]<<"  ";
+                        cout<<COMPUTERS_BOARD[r][c]<<"  ";
                 }
                 cout<<endl;
         }
-
-}*/
-
-
-
+}
 
 //Initialize default variables
 //Create game board
