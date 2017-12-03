b7c4975d56f0ce624f37a0d84e58356bf142e0a5
Malnassafi <malnassafi@mail.csuchico.edu>
2017-12-03 14:07:41 -0800

    HeaderFile.

--- battleship.h
+++ battleship.h
@@ -8,33 +8,34 @@
 
 using namespace std;
 
+const int NUM_OF_ROWS = 10;
+const int NUM_OF_COLS = 10;
+const int MAX_NUM_OF_SHIPS = 8;
+
+const char IS_HIT = '!';
+const char IS_WATER = '_';
+const char IS_MISSED = 'O';
+const char IS_SHIP = 'S';
+
 class board
 {
         private:
                 string playerName; 
                 string computer;
-                int NUM_OF_ROWS;
-                int NUM_OF_COLS;
-                int MAX_NUM_OF_SHIPS;
                 
                 char PLAYER_BOARD[NUM_OF_ROWS][NUM_OF_COLS];
                 char COMPUTERS_BOARD[NUM_OF_ROWS][NUM_OF_COLS];
                 
-                char IS_HIT;
-                char IS_WATER;
-                char IS_MISSED;
-                char IS_SHIP;        
-                
                 void placeYourShips(char array[NUM_OF_ROWS][NUM_OF_COLS]);
                 void computerRandomGenerateShips(char array[NUM_OF_ROWS][NUM_OF_COLS]);
                 void playerAttack(char array[NUM_OF_ROWS][NUM_OF_COLS]);
-                void computerRandomGeneratedAttack(char array[NUM_OF_ROWS][NUM_OF_COLS],);
+                void computerRandomGeneratedAttack(char array[NUM_OF_ROWS][NUM_OF_COLS]);
                 void displayGameBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string);
                 int numOfShips(char array[NUM_OF_ROWS][NUM_OF_COLS]);
         public:
                 board();
                 void displayBoard(string);
-                int numberOfShips(string);
+                int  numberOfShips(string);
                 void setName(string);
                 void attack(string);
                 void placeShips(string);
