650c5f2d6da7c5b85522aaaad6036d61bfef3781
Malnassafi <malnassafi@mail.csuchico.edu>
2017-12-07 14:30:41 -0800

    HeaderFile.

--- battleship.h
+++ battleship.h
@@ -8,10 +8,11 @@
 
 using namespace std;
 
+// NO MAGIC NUMBERS!
 const int NUM_OF_ROWS = 10;
 const int NUM_OF_COLS = 10;
 const int MAX_NUM_OF_SHIPS = 8;
-
+// Const char's
 const char IS_HIT = '!';
 const char IS_WATER = '_';
 const char IS_MISSED = 'O';
@@ -19,6 +20,7 @@ const char IS_SHIP = 'S';
 
 class board
 {
+        // Private section where int main cant use them but the public section can
         private:
                 string playerName; 
                 string computer;
@@ -33,6 +35,8 @@ class board
                 void computerRandomGeneratedAttack(char array[NUM_OF_ROWS][NUM_OF_COLS]);
                 void displayGameBoard(char  array[NUM_OF_ROWS][NUM_OF_COLS], string);
                 int numOfShips(char array[NUM_OF_ROWS][NUM_OF_COLS]);
+                void setBoard(char array[NUM_OF_ROWS][NUM_OF_COLS]); 
+        // Public section where they can be used in int main
         public:
                 board();
                 void displayBoard(string);
